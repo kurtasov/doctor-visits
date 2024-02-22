8142cf49062e1bc5eec02c91bf143a31083b77ff
kurtasov <kurtasov@users.noreply.github.com>
2024-02-22 19:43:55 +0300

    added Query Helper

---
+++ queryhelper.py
@@ -0,0 +1,19 @@
+from pony.orm import *
+from datetime import datetime
+import models
+
+
+@db_session
+def lookup_patient(birth_date):
+    birth_date_obj = datetime.strptime(birth_date, '%Y-%m-%d').date()
+    q = models.Patient.select(lambda p: p.date_of_birth == birth_date_obj)
+    if q.count() > 1:
+        raise Exception("Patient not found")
+
+    q.show()
+
+
+@db_session
+def lookup_doctor(last_name):
+    q = models.Doctor.select(lambda d: d.last_name == last_name)
+    q.show()
